29025e3a2c66a5d893ee1eaaca7b8a6b8e1cd3af
TamNguyen2312 <th3m1nd05@gmail.com>
2024-09-17 01:12:25 +0700

    Create MappingProfile.cs

---
+++ Task2.BLL/Helpers/Mapper/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.BLL.Helpers.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
