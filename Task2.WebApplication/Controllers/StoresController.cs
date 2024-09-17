ecf5ae554737130c1f3a9d6d303ea5518c85ff87
TamNguyen2312 <th3m1nd05@gmail.com>
2024-09-18 01:14:59 +0700

    add StoresController and View Index

---
+++ Task2.WebApplication/Controllers/StoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task2.WebApplicationMVC.Controllers
+{
+	public class StoresController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
